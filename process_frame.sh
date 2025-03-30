e50da5c832ca14fb49e37a4547f1362c37fc63ca
yymin1022 <yymin1022@gmail.com>
2025-03-30 13:07:04 +0000

    Remvoed process_frame.sh : Delete redundant script

--- process_frame.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Processing Frames..."
-python3 Image_Processing/process.py "resources/frame_h.jpg" "resources/frame_h_rev.bmp" 320 40
-python3 Image_Processing/process.py "resources/frame_f.jpg" "resources/frame_f_rev.bmp" 320 255
-echo "Process Frames Done..!"
